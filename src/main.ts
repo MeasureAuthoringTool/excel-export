741ad03928ca22efe764c4bb9b8c7a1723d214fd
Greg Akins <gregory.akins@icf.com>
2024-04-11 08:31:56 -0400

    MAT-6729: Adding Cors attribute to allow the service to be called from our front end

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { ExportModule } from './export.module';
 
 export async function bootstrap() {
-  const app = await NestFactory.create(ExportModule);
+  const app = await NestFactory.create(ExportModule, { cors: true });
   await app.listen(3000);
 }
 bootstrap();
